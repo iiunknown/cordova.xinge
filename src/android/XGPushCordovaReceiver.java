350af0445b890c6e5bd547f0811b2fb9829d8c87
YangLiu <iiunknown@gmail.com>
2014-11-10 14:18:29 +0800

    add message in XG Receiver for Android platform.

--- src/android/XGPushCordovaReceiver.java
+++ src/android/XGPushCordovaReceiver.java
@@ -22,31 +22,97 @@ public class XGPushCordovaReceiver extends XGPushBaseReceiver {
 	public static  CallbackContext msgCallbackContext =null;
 	public static  CallbackContext openCallbackContext =null;
 
+	
+	public  String getTopActivityName(Context context){
+        String topActivityClassName=null;
+         ActivityManager activityManager =
+        (ActivityManager)(context.getSystemService(android.content.Context.ACTIVITY_SERVICE )) ;
+         List<RunningTaskInfo> runningTaskInfos = activityManager.getRunningTasks(1) ;
+         if(runningTaskInfos != null){
+             ComponentName f=runningTaskInfos.get(0).topActivity;
+             topActivityClassName=f.getClassName();
+         }
+         return topActivityClassName;
+    }
+
 	public void onTextMessage(Context context, XGPushTextMessage message) {
 		if (context == null || message == null) {
 			return;
 		}
-		String text = "onTextMessage:" + message.toString();
-		Map<String,Object> map = new HashMap<String,Object>();
-		map.put("xtype", "message");
-		map.put("title", message.getTitle());
-		map.put("content", message.getContent());
-		JSONObject msg = new JSONObject(map);
-		JSONObject customObj;
+		//消息内容
+		String content = message.getContent();
+		//消息标题
+		String title = message.getTitle();
+		//消息自定义kv值
+		JSONObject customObj = null;
 		try {
-			String content =message.getCustomContent();
-			if(content!=null&&!content.trim().equals("")) {
+			String customContent =message.getCustomContent();
+			if(customContent!=null&&!customContent.trim().equals("")) {
 				customObj = new JSONObject(message.getCustomContent());
-				msg.putOpt("custom", customObj);
 			}
 		} catch (JSONException e) {
 			Log.e(LogTag, "create json falied", e);
 		}
-		Log.d(LogTag, text);
-		if(msgCallbackContext!=null) {
-			PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, msg);
-			pluginResult.setKeepCallback(true);
-			msgCallbackContext.sendPluginResult(pluginResult);
+		
+		//表示app正在运行								
+		if (getTopActivityName(context).equals("com.shuishantech.s2mobile.s2mobile")){
+			
+			if(title.equals("shuishan.s2.apps.chat.newmessage")){
+				if(msgCallbackContext!=null) {
+					PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, content);
+					pluginResult.setKeepCallback(true);
+					msgCallbackContext.sendPluginResult(pluginResult);
+				}
+			} else if (title.equals("shuishan.s2.apps.workflow.newmessage")){
+				if(msgCallbackContext!=null) {
+					PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, content);
+					pluginResult.setKeepCallback(true);
+					msgCallbackContext.sendPluginResult(pluginResult);
+				}
+			}
+    	}
+		//app在后台运行或者已经被kill
+		else{
+			//收到新消息
+			if(title.equals("shuishan.s2.apps.chat.newmessage")){
+				try {
+					content = java.net.URLDecoder.decode(content , "utf-8");
+					JSONObject contentObject = new JSONObject(content);
+					String userName = contentObject.getString("sender");
+					String messageContent = contentObject.getString("content");
+					XGLocalMessage localMessage = new XGLocalMessage();
+					localMessage.setContent(messageContent);
+					localMessage.setTitle(userName);
+					localMessage.setType(1);
+					XGPushManager.clearLocalNotifications(context);
+			        Long msgId = XGPushManager.addLocalNotification(context, localMessage);
+				} catch (UnsupportedEncodingException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (JSONException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			} else if (title.equals("shuishan.s2.apps.workflow.newmessage")){
+				try {
+					content = java.net.URLDecoder.decode(content , "utf-8");
+					JSONObject contentObject = new JSONObject(content);
+					String messageTitle = contentObject.getString("title");
+					String messageContent = contentObject.getString("content");
+					XGLocalMessage localMessage = new XGLocalMessage();
+					localMessage.setContent(messageContent);
+					localMessage.setTitle(messageTitle);
+					localMessage.setType(1);
+					XGPushManager.clearLocalNotifications(context);
+			        Long msgId = XGPushManager.addLocalNotification(context, localMessage);
+				} catch (UnsupportedEncodingException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (JSONException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
 		}
 	}
 	@Override
