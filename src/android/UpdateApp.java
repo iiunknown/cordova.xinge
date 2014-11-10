508d7d253c65f907faf7b64a61bb3ae7875a1c2d
YangLiu <iiunknown@gmail.com>
2014-11-10 18:19:07 +0800

    Android update

--- src/android/UpdateApp.java
+++ src/android/UpdateApp.java
@@ -3,10 +3,15 @@ package com.iiunknown.cordova.xinge;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
+
+
+
 
 
 
@@ -17,7 +22,6 @@ import org.apache.cordova.CordovaPlugin;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import android.app.AlertDialog;
 import android.app.AlertDialog.Builder;
 import android.app.Dialog;
@@ -34,177 +38,178 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ProgressBar;
 
+
 public class UpdateApp extends CordovaPlugin {
-	
-	/*�汾�ż��·��*/
-	private String checkPath;
-	/* �°汾��*/
-	private int newVerCode;
-	/* �°汾��� */
-	private String newVerName;
-	/* APK ����·��*/
-	private String  downloadPath;
-	/* ������ */
+    
+    /*版本号检查路径*/
+    private String checkPath;
+    /* 新版本号*/
+    private int newVerCode;
+    /* 新版本名称 */
+    private String newVerName;
+    /* APK 下载路径*/
+    private String  downloadPath;
+    /* 下载中 */
     private static final int DOWNLOAD = 1;
-    /* ���ؽ��� */
+    /* 下载结束 */
     private static final int DOWNLOAD_FINISH = 2;
-    /* ���ر���·�� */
+    /* 下载保存路径 */
     private String mSavePath;
-    /* ��¼��������� */
+    /* 记录进度条数量 */
     private int progress;
-    /* �Ƿ�ȡ����� */
+    /* 是否取消更新 */
     private boolean cancelUpdate = false;
-    /* ������*/
+    /* 上下文*/
     private Context mContext;
-    /* ���½���� */
+    /* 更新进度条 */
     private ProgressBar mProgress;
     private Dialog mDownloadDialog;
-	
-	@Override
+    
+    @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         this.mContext = cordova.getActivity();
-		if (action.equals("checkAndUpdate")) {
-			this.checkPath = args.getString(0);
-//			callbackContext.error(Environment.getDataDirectory().getPath());;
-        	checkAndUpdate(callbackContext);
-        	return true;
+        if (action.equals("checkAndUpdate")) {
+            this.checkPath = args.getString(0);
+//          callbackContext.error(Environment.getDataDirectory().getPath());;
+            checkAndUpdate(callbackContext);
+            return true;
         }else if(action.equals("getCurrentVersion")){
-        	callbackContext.success(this.getCurrentVerCode(callbackContext)+"");
+            callbackContext.success(this.getCurrentVerCode(callbackContext)+"");
         }else if(action.equals("getCurrentVersionName")){
-        	callbackContext.success(this.getCurrentVerName());
+            callbackContext.success(this.getCurrentVerName());
         }else if(action.equals("getServerVersion")){
-        	this.checkPath = args.getString(0);
-        	if(this.getServerVerInfo(callbackContext)){
-        		callbackContext.success(newVerCode+"");
-        	}else{
-        		callbackContext.error("can't connect to the server!please check [checkpath]");
-        	}
-        	
+            this.checkPath = args.getString(0);
+            if(this.getServerVerInfo(callbackContext)){
+                callbackContext.success(newVerCode+"");
+            }else{
+                callbackContext.error("can't connect to the server!please check [checkpath]");
+            }
+            
         }
         return false;
     }
-	
+    
 
     
     /**
-     * ������
+     * 检查更新
      */
     private void checkAndUpdate(CallbackContext callbackContext){
-    	try
-    	{
-    	if(getServerVerInfo(callbackContext)){
-    		int currentVerCode = getCurrentVerCode(callbackContext);
-    		if(newVerCode>currentVerCode){
-    			this.showNoticeDialog();
-    		}
-    	}
-    	}catch(Exception e)
-    	{
-			callbackContext.error(e.getMessage());
-    	}
+        try
+        {
+        if(getServerVerInfo(callbackContext)){
+            int currentVerCode = getCurrentVerCode(callbackContext);
+            if(newVerCode>currentVerCode){
+                this.showNoticeDialog();
+            }
+        }
+        }catch(Exception e)
+        {
+            callbackContext.error(e.getMessage());
+        }
     }
-	
-	
-	/**
-     * ��ȡӦ�õ�ǰ�汾����
+    
+    
+    /**
+     * 获取应用当前版本代码
      * @param context
      * @return
      */
     private int getCurrentVerCode(CallbackContext callbackContext){
 
-    	int currentVer = -1;
-    	try {
-        	String packageName = this.mContext.getPackageName();
-			currentVer = this.mContext.getPackageManager().getPackageInfo(packageName, 0).versionCode;
-		} catch (Exception e) {
-			callbackContext.error(e.getMessage());
-			e.printStackTrace();
-		}
-    	return currentVer;
+        int currentVer = -1;
+        try {
+            String packageName = this.mContext.getPackageName();
+            currentVer = this.mContext.getPackageManager().getPackageInfo(packageName, 0).versionCode;
+        } catch (Exception e) {
+            callbackContext.error(e.getMessage());
+            e.printStackTrace();
+        }
+        return currentVer;
     }
     
     /**
-     * ��ȡӦ�õ�ǰ�汾���
+     * 获取应用当前版本名称
      * @param context
      * @return
      */
     private String getCurrentVerName(){
-    	String packageName = this.mContext.getPackageName();
-    	String currentVerName = "";
-    	try {
-    		currentVerName = this.mContext.getPackageManager().getPackageInfo(packageName, 0).versionName;
-		} catch (NameNotFoundException e) {
-			e.printStackTrace();
-		}
-    	return currentVerName;
+        String packageName = this.mContext.getPackageName();
+        String currentVerName = "";
+        try {
+            currentVerName = this.mContext.getPackageManager().getPackageInfo(packageName, 0).versionName;
+        } catch (NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return currentVerName;
     }
         
     /**
-     * ��ȡӦ�����
+     * 获取应用名称
      * @param context
      * @return
      */
     private String getAppName(){
-    	return this.mContext.getResources().getText(R.string.app_name).toString();
+        return this.mContext.getResources().getText(string.app_name).toString();
     }
     
     /**
-     * ��ȡ�������ϵİ汾��Ϣ
+     * 获取服务器上的版本信息
      * @param path
      * @return
      * @throws Exception
      */
     private boolean getServerVerInfo(CallbackContext callbackContext){
-		try {
-			StringBuilder verInfoStr = new StringBuilder();
-			URL url = new URL(checkPath);
-			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-			conn.setConnectTimeout(5000);
-			conn.setReadTimeout(5000);
-			conn.connect();
-			BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream(),"UTF-8"),8192);
-			String line = null;
-			while((line = reader.readLine()) != null){
-				verInfoStr.append(line+"\n");
-			}
-			reader.close();
-			
-			JSONArray array = new JSONArray(verInfoStr.toString());
-			if(array.length()>0){
-				JSONObject obj = array.getJSONObject(0);
-				newVerCode = obj.getInt("verCode");
-				newVerName = obj.getString("verName");
-				downloadPath = obj.getString("apkPath");
-			}
-		} catch (Exception e) {
-			callbackContext.error(e.getMessage());
-			e.printStackTrace();
-			return false;
-		} 
-    	
-    	return true;
-    	
+        try {
+            StringBuilder verInfoStr = new StringBuilder();
+            URL url = new URL(checkPath);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setConnectTimeout(5000);
+            conn.setReadTimeout(5000);
+            conn.connect();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream(),"UTF-8"),8192);
+            String line = null;
+            while((line = reader.readLine()) != null){
+                verInfoStr.append(line+"\n");
+            }
+            reader.close();
+            
+            JSONArray array = new JSONArray(verInfoStr.toString());
+            if(array.length()>0){
+                JSONObject obj = array.getJSONObject(0);
+                newVerCode = obj.getInt("verCode");
+                newVerName = obj.getString("verName");
+                downloadPath = obj.getString("apkPath");
+            }
+        } catch (Exception e) {
+            callbackContext.error(e.getMessage());
+            e.printStackTrace();
+            return false;
+        } 
+        
+        return true;
+        
     }
     
     /**
-     * ��ʾ������¶Ի���
+     * 显示软件更新对话框
      */
     private void showNoticeDialog() {
-    	
-        // ����Ի���
+        
+        // 构造对话框
         AlertDialog.Builder builder = new Builder(mContext);
-        builder.setTitle(R.string.soft_update_title);
-        builder.setMessage(R.string.soft_update_info);
-        // ����
-        builder.setPositiveButton(R.string.soft_update_updatebtn, new OnClickListener(){
+        builder.setTitle(string.soft_update_title);
+        builder.setMessage(string.soft_update_info);
+        // 更新
+        builder.setPositiveButton(string.soft_update_updatebtn, new OnClickListener(){
             public void onClick(DialogInterface dialog, int which){
                 dialog.dismiss();
-                // ��ʾ���ضԻ���
+                // 显示下载对话框
                 showDownloadDialog();
             }
         });
-        // �Ժ����
-        builder.setNegativeButton(R.string.soft_update_later, new OnClickListener(){
+        // 稍后更新
+        builder.setNegativeButton(string.soft_update_later, new OnClickListener(){
             public void onClick(DialogInterface dialog, int which){
                 dialog.dismiss();
             }
@@ -214,49 +219,39 @@ public class UpdateApp extends CordovaPlugin {
     }
 
     /**
-     * ��ʾ������ضԻ���
+     * 显示软件下载对话框
      */
     private void showDownloadDialog()
     {
-        // ����������ضԻ���
+        // 构造软件下载对话框
         AlertDialog.Builder builder = new Builder(mContext);
-        builder.setTitle(R.string.soft_updating);
-        // �����ضԻ������ӽ����
+        builder.setTitle(string.soft_updating);
+        // 给下载对话框增加进度条
         final LayoutInflater inflater = LayoutInflater.from(mContext);
-        View v = inflater.inflate(R.layout.softupdate_progress, null);
-        mProgress = (ProgressBar) v.findViewById(R.id.update_progress);
+        View v = inflater.inflate(layout.softupdate_progress, null);
+        mProgress = (ProgressBar) v.findViewById(id.update_progress);
         builder.setView(v);
-        // ȡ�����
+        // 取消更新
         builder.setNegativeButton(R.string.soft_update_cancel, new OnClickListener() {
             public void onClick(DialogInterface dialog, int which) {
                 dialog.dismiss();
-                // ����ȡ��״̬
+                // 设置取消状态
                 cancelUpdate = true;
             }
         });
         mDownloadDialog = builder.create();
         mDownloadDialog.show();
-        // �����ļ�
+        // 现在文件
         downloadApk();
     }
 
     /**
-     * ����apk�ļ�
+     * 下载apk文件
      */
     private void downloadApk()
     {
-        // �������߳��������
-//    	try
-//    	{
-		new downloadApkThread().start();
-//    	}
-//    	catch (Exception e)
-//    	{
-//            AlertDialog.Builder builder = new Builder(this.mContext);
-//            builder.setTitle(e.getMessage());
-//            errorDialog = builder.create();
-//            errorDialog.show();
-//    	}
+        // 启动新线程下载软件
+        new downloadApkThread().start();
     }
 
     private Handler mHandler = new Handler()
@@ -265,13 +260,13 @@ public class UpdateApp extends CordovaPlugin {
         {
             switch (msg.what)
             {
-            // ��������
+            // 正在下载
             case DOWNLOAD:
-                // ���ý����λ��
+                // 设置进度条位置
                 mProgress.setProgress(progress);
                 break;
             case DOWNLOAD_FINISH:
-                // ��װ�ļ�
+                // 安装文件
                 installApk();
                 break;
             default:
@@ -281,88 +276,80 @@ public class UpdateApp extends CordovaPlugin {
     };
     
     /**
-     * �����ļ��߳�
+     * 下载文件线程
      */
-	private class downloadApkThread extends Thread {
-		@Override
-		public void run() {
-			try {
+    private class downloadApkThread extends Thread {
+        @Override
+        public void run() {
+            try {
+                // 判断SD卡是否存在，并且是否具有读写权限
+                //if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) 
+                {
+                    // 获得存储卡的路径
+                    String sdpath = Environment.getExternalStorageDirectory()+ "/";
+                    mSavePath = sdpath + "download";
+                    URL url = new URL(downloadPath);
+                    // 创建连接
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    conn.connect();
+                    // 获取文件大小
+                    int length = conn.getContentLength();
+                    // 创建输入流
+                    InputStream is = conn.getInputStream();
 
-				// �ж�SD���Ƿ���ڣ������Ƿ���ж�дȨ��
-				//Environment.getDownloadCacheDirectory()
-				//if (Environment.getDataDirectory().getPath() != "") 
-				{
-					// ��ô洢����·��
+                    File file = new File(mSavePath);
+                    // 判断文件目录是否存在
+                    if (!file.exists()) {
+                        file.mkdir();
+                    }
+                    File apkFile = new File(mSavePath, newVerName);
+                    FileOutputStream fos = new FileOutputStream(apkFile);
+                    int count = 0;
+                    // 缓存
+                    byte buf[] = new byte[1024];
+                    // 写入到文件中
+                    do {
+                        int numread = is.read(buf);
+                        count += numread;
+                        // 计算进度条位置
+                        progress = (int) (((float) count / length) * 100);
+                        // 更新进度
+                        mHandler.sendEmptyMessage(DOWNLOAD);
+                        if (numread <= 0) {
+                            // 下载完成
+                            mHandler.sendEmptyMessage(DOWNLOAD_FINISH);
+                            break;
+                        }
+                        // 写入文件
+                        fos.write(buf, 0, numread);
+                    } while (!cancelUpdate);// 点击取消就停止下载.
+                    fos.close();
+                    is.close();
+                }
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            // 取消下载对话框显示
+            mDownloadDialog.dismiss();
+        }
+    };
 
-					String sdpath = Environment.getExternalStorageDirectory()+ "/";
-					mSavePath = sdpath + "download";
-					URL url = new URL(downloadPath);
-					// ��������
-					HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-					conn.connect();
-					// ��ȡ�ļ���С
-					int length = conn.getContentLength();
-					// ����������
-					InputStream is = conn.getInputStream();
-					File file = new File(mSavePath);
-					// �ж��ļ�Ŀ¼�Ƿ����
-					if (!file.exists()) {
-						file.mkdir();
-					}
-					String fileName = mSavePath + "/" + newVerName;
-					File apkFile = new File(fileName);
-					if (!apkFile.exists()) {
-						apkFile.createNewFile();
-					}
-					FileOutputStream fos = new FileOutputStream(apkFile);
-					int count = 0;
-					// ����
-					byte buf[] = new byte[1024];
-					// д�뵽�ļ���
-					do {
-						int numread = is.read(buf);
-						count += numread;
-						// ��������λ��
-						progress = (int) (((float) count / length) * 100);
-						// ���½��
-						mHandler.sendEmptyMessage(DOWNLOAD);
-						if (numread <= 0) {
-							// �������
-							mHandler.sendEmptyMessage(DOWNLOAD_FINISH);
-							break;
-						}
-						// д���ļ�
-						fos.write(buf, 0, numread);
-					} while (!cancelUpdate);// ���ȡ���ֹͣ����.
-					fos.close();
-					is.close();
-					progress = 100;
-				}
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-
-			// ���½��
-			mHandler.sendEmptyMessage(DOWNLOAD);
-			// ȡ�����ضԻ�����ʾ
-			mDownloadDialog.dismiss();
-		}
-	};
-
-	/**
-	 * ��װAPK�ļ�
-	 */
-	private void installApk() {
-		File apkfile = new File(mSavePath, newVerName);
-		if (!apkfile.exists()) {
-			return;
-		}
-		// ͨ��Intent��װAPK�ļ�
-		Intent i = new Intent(Intent.ACTION_VIEW);
-		i.setDataAndType(Uri.parse("file://" + apkfile.toString()),
-				"application/vnd.android.package-archive");
-		mContext.startActivity(i);
-	}
+    /**
+     * 安装APK文件
+     */
+    private void installApk() {
+        File apkfile = new File(mSavePath, newVerName);
+        if (!apkfile.exists()) {
+            return;
+        }
+        // 通过Intent安装APK文件
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setDataAndType(Uri.parse("file://" + apkfile.toString()),
+                "application/vnd.android.package-archive");
+        mContext.startActivity(i);
+    }
     
 
 }
