fe648a9c1dc8583e2739b82039f2200c5f06d6ca
YangLiu <iiunknown@gmail.com>
2014-11-18 22:23:50 +0800

    update Android

--- www/xinge.js
+++ www/xinge.js
@@ -1,75 +1,177 @@
-var xingePlugin = {};
+package com.iiunknown.cordova.xinge;
 
-// XGPushManager功能类方法代理开始
-//启动并注册APP，同时绑定账号。
-xingePlugin.register = function (account, successCallback, errorCallback) {
-    cordova.exec(successCallback, errorCallback, "Xinge", "register", [account]);
-};
-//反注册，建议在不需要接收推送的时候调用。
-xingePlugin.unregister = function(successCallback, errorCallback){
-    cordova.exec(successCallback, errorCallback, "Xinge", "unregister", []);
-};
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.PluginResult;
+import org.json.JSONArray;
+import org.json.JSONException;
 
-// XGPushManager功能类方法代理结束
+import android.content.Context;
+import android.util.Log;
 
-//XGPushConfig配置类开始
-//配置accessId
-xingePlugin.setAccessId = function(accessId){
-    cordova.exec(null, null, "Xinge", "setAccessId", [accessId]);
-}
-//配置accessKey
-xingePlugin.setAccessKey = function(accessKey){
-    cordova.exec(null, null, "Xinge", "setAccessKey", [accessKey]);
-}
-//获取设备的token，只有注册成功才能获取到正常的结果
-xingePlugin.getToken = function(successCallback, errorCallback){
-    cordova.exec(successCallback, errorCallback, "Xinge", "getToken", []);
-}
-//配置accessId和accessKey
-xingePlugin.config = function(accessId, accessKey, successCallback, errorCallback){
-    cordova.exec(successCallback, errorCallback, "Xinge", "config", [accessId, accessKey]);
-};
-//获取当前应用包名
-xingePlugin.getPackageName = function(successCallback, errorCallback){
-    cordova.exec(successCallback, errorCallback, "Xinge", "getPackageName", []);
-};
-//获取当前应用版本号
-xingePlugin.getVersion = function(successCallback, errorCallback){
-    cordova.exec(successCallback, errorCallback, "Xinge", "getVersion", []);
-};
-//XGPushConfig配置类结束
+import com.tencent.android.tpush.*;
 
+public class Xinge extends CordovaPlugin {
 
-xingePlugin.onMessage = function (successCallback, errorCallback) {
-    if (errorCallback == null) {
-        errorCallback = function () {
-        }
-    }
+    private Context mContext;
 
-    if (typeof successCallback != "function") {
-        console.log("Xinge.onMessage failure: success callback parameter must be a function");
-        return;
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        this.mContext = cordova.getActivity();
+        Log.d("TPush", "execute action:"+action+" with args:"+args);
+         if ("register".equals(action)) {
+             return register(args, callbackContext);
+         }
+         else if ("unregister".equals(action)){
+             return unregister(callbackContext);
+         }
+         else if ("setAccessId".equals(action)){
+            return setAccessId(args);
+         }
+         else if ("setAccessKey".equals(action)){
+            return setAccessKey(args);
+         }
+         else if ("config".equals(action)){
+            return config(args, callbackContext);
+         }
+         else if ("getToken".equals(action)){
+            return getToken(callbackContext);
+         }
+         else if("onMessage".equals(action)) {
+             return onMessage(callbackContext);
+         }
+         else if("notify".equals(action)) {
+             String title = args.getString(0);
+             String content = args.getString(1);
+             return notify(title,content,callbackContext);
+         }
+         else if ("getPackageName".equals(action)){
+             return getPackageName(callbackContext);
+         }
+         else if ("getVersion".equals(action)){
+             return getVersion(callbackContext);
+         }
+         return false;
     }
     
-    cordova.exec(successCallback, errorCallback, "Xinge", "onMessage", []);
-};
-xingePlugin.notify = function (title,content,successCallback, errorCallback) {
-    if (errorCallback == null) {
-        errorCallback = function () {
-        }
+
+    public boolean register(JSONArray args,final CallbackContext callbackContext){
+        try{
+            int count = args.length();
+            if (count == 0){
+                XGPushManager.registerPush(this.cordova.getActivity());
+            }
+            else if (count == 1){
+                String account = args.getString(0);
+                XGPushManager.registerPush(this.cordova.getActivity(), account);
+            }
+            callbackContext.success();
+        } catch(Exception e) {
+            callbackContext.error("Exception: " + e.getMessage());
+            System.err.println("Exception: " + e.getMessage());
+            return false;
+        } 
+        return true;
     }
-    if (successCallback == null) {
-        successCallback = function (data) {
-            console.log("notify "+title+":"+data);
-        }
+
+    public boolean unregister(final CallbackContext callbackContext) {
+        XGPushManager.unregisterPush(this.cordova.getActivity());
+        callbackContext.success();
+        Log.d("TPush", "unregister push sucess");
+        return true;
     }
-    cordova.exec(successCallback, errorCallback, "Xinge", "notify", [title,content]);
-};
 
-//for iOS only
-xingePlugin.registerDevice = function(successCallback, errorCallback){
-    cordova.exec(successCallback, errorCallback, "Xinge", "registerDevice", []);
-};
+    public boolean config(JSONArray args, final CallbackContext callbackContext){
+        try{
+            Long accessId = args.getLong(0);
+            String accessKey = args.getString(1);
+            XGPushConfig.setAccessId(this.cordova.getActivity(), accessId);
+            XGPushConfig.setAccessKey(this.cordova.getActivity(), accessKey);
+            XGPushManager.onActivityStarted(this.cordova.getActivity());
+            callbackContext.success();
+        }
+        catch(Exception e){
+            callbackContext.error("Exception: " + e.getMessage());
+            System.err.println("Exception: " + e.getMessage());
+            return false;
+        }
+        return true;
+    }
 
-module.exports = xingePlugin;
+    public boolean setAccessId(JSONArray args){
+        try{
+            long id = args.getLong(0);
+            XGPushConfig.setAccessId(this.cordova.getActivity(), id);
+        } catch(Exception e) {
+            System.err.println("Exception: " + e.getMessage());
+            return false;
+        } 
+        return true;
+    }
 
+    public boolean setAccessKey(JSONArray args){
+        try{
+            String key = args.getString(0);
+            XGPushConfig.setAccessKey(this.cordova.getActivity(), key);
+        } catch(Exception e) {
+            System.err.println("Exception: " + e.getMessage());
+            return false;
+        } 
+        return true;
+    }
+
+    public boolean getToken(final CallbackContext callbackContext){
+        try{
+            String token = XGPushConfig.getToken(this.cordova.getActivity());
+            callbackContext.success(""+token);
+        } catch (Exception e){
+            System.err.println("Exception: " + e.getMessage());
+            callbackContext.error(""+e.getMessage());
+            return false;
+        }
+        return true;
+    }
+
+
+    public boolean onMessage(final CallbackContext callbackContext) {
+        XGPushCordovaReceiver.msgCallbackContext = callbackContext;
+        PluginResult pluginResult = new PluginResult(PluginResult.Status.NO_RESULT);
+        pluginResult.setKeepCallback(true);
+        callbackContext.sendPluginResult(pluginResult);
+        return true;
+    }
+
+    public boolean notify(String title,String content,final CallbackContext callbackContext) {
+        XGLocalMessage localMessage = new XGLocalMessage();
+        localMessage.setContent(content);
+        localMessage.setTitle(title);
+        localMessage.setType(1);
+        //localMessage.setBuilderId(XGPushConfig.);
+        Long msgId = XGPushManager.addLocalNotification(this.cordova.getActivity(),localMessage);
+        callbackContext.success(""+msgId);
+        return true;
+    }
+
+    public boolean getVersion(final CallbackContext callbackContext){
+        try{
+            String packageName = this.mContext.getPackageName();
+            String currentVerName = this.mContext.getPackageManager().getPackageInfo(packageName, 0).versionName;
+            callbackContext.success(""+currentVerName);
+        } catch (Exception e){
+            System.err.println("Exception: " + e.getMessage());
+            callbackContext.error(""+e.getMessage());
+            return false;
+        }
+        return true;
+    }
+    public boolean getPackageName(final CallbackContext callbackContext){
+        try{
+            String packageName = this.mContext.getPackageName();
+            callbackContext.success(""+packageName);
+        } catch (Exception e){
+            System.err.println("Exception: " + e.getMessage());
+            callbackContext.error(""+e.getMessage());
+            return false;
+        }
+        return true;
+    }
+}
